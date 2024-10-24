141c77d082b7afcec792b6401d793ce13c314775
SaswatSankalpParida <saswatsankalp@gmail.com>
2024-10-24 09:19:31 +0530

    Create proj3.c

---
+++ proj3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, difference ;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    difference = num1 - num2;
+    printf("The sum of %d and %d is: %d\n", num1, num2, difference);
+    return 0;
+}
